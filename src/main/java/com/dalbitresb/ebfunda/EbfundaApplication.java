4d417a90fc6a64e80878e785d67d7a10b95a735a
Diego Albitres <diego.albitres@gmail.com>
2023-07-03 11:07:10 -0500

    Initial commit

---
+++ src/main/java/com/dalbitresb/ebfunda/EbfundaApplication.java
@@ -0,0 +1,13 @@
+package com.dalbitresb.ebfunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbfundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbfundaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalbitresb/ebfunda/EbfundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalbitresb.ebfunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbfundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
